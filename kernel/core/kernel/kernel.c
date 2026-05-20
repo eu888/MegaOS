a712c7d6a66de56dde6a972978fbec03f3c7657e
Victor <albu.victor.m@gmail.com>
2026-05-20 09:24:59 +0300

    Add kernel stack allocation and setup in kernel_main and fixed some errors so we get kernel panic instead of guru meditation.

--- kernel/core/kernel/kernel.c
+++ kernel/core/kernel/kernel.c
@@ -13,6 +13,9 @@
         init_heap();
         void* user_stack = kmalloc(4096);
         uintptr_t user_stack_top = (uintptr_t)user_stack + 4096;
+        void* kernel_stack = kmalloc(4096);
+        uintptr_t kernel_stack_top = (uintptr_t)kernel_stack + 4096;
+        set_kernel_stack(kernel_stack_top);
         jump_usermode(user_stack_top);
         terminal_write_string("um\n");
         void print_start_symbol();
